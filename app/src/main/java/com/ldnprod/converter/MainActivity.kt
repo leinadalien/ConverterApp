48ed40e18c34480694c5a5775c46ba87a141d029
leinadalien <danila.sokolyuk@gmail.com>
2022-10-19 05:31:48 +0300

    add design of buttons

--- app/src/main/java/com/ldnprod/converter/MainActivity.kt
+++ app/src/main/java/com/ldnprod/converter/MainActivity.kt
@@ -1,26 +1,37 @@
 package com.ldnprod.converter
 
+import android.app.ActionBar.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.gridlayout.widget.GridLayout
 
 class MainActivity : AppCompatActivity() {
-    private var data = arrayOf("a", "b", "c", "d", "e")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var gridlayout:GridLayout = findViewById(R.id.grid_layout)
-        for (i in 1..8) {
-            var button = layoutInflater.inflate(R.layout.num_button, gridlayout, false) as Button
-            button.text = i.toString()
+        initButtons()
 
+    }
+
+    private fun initButtons(){
+        val gridlayout:GridLayout = findViewById(R.id.grid_layout)
+        var button:Button
+        for (i in 1..9) {
+            button = layoutInflater.inflate(R.layout.num_button, gridlayout, false) as Button
+            button.text = i.toString()
             gridlayout.addView(button)
         }
-
-
-
+        button = layoutInflater.inflate(R.layout.num_button, gridlayout, false) as Button
+        button.text = "0"
+        gridlayout.addView(button)
+        button = layoutInflater.inflate(R.layout.num_button, gridlayout, false) as Button
+        button.text = "."
+        gridlayout.addView(button)
+        button = layoutInflater.inflate(R.layout.num_button, gridlayout, false) as Button
+        button.text = "*"
+        gridlayout.addView(button)
     }
 }
